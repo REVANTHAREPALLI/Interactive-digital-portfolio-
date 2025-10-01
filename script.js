30ce495e4e56bddc35cf41232eb2c05acba57112
REVANTH AREPALLI <revantharepalli10@gmail.com>
2025-10-01 20:37:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll (extra feature)
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Simple console message
+console.log("Portfolio Loaded Successfully 🚀");
